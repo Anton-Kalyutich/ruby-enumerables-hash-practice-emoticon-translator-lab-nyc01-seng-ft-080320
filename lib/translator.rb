2b845c810fe5c2b2be1b978800b47820c1bf2fbc
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-21 23:23:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,8 @@ def get_english_meaning(filename, emoticon)
     dictionary.each do |key, value|
       if value == emoticon
         return emotion
+      else
+        return 
       end
     end
   end
