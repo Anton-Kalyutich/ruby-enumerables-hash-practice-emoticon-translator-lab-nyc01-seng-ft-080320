8f811d3e98935c18f3f1a2c16b7f1b6f31bb9b18
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-21 23:35:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,6 +30,7 @@ def get_japanese_emoticon(filename, emoticon)
     if the_hash[emotion][:english] == emoticon
       return the_hash[emotion][:japanese]
     end
+  end
   
   
   return "Sorry, that emoticon was not found"
